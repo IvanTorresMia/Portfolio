b97d988ef8a7f153b96d678893ae23f411bd4fdd
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-10 04:28:16 -0800

    ading styling for navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
  <div>
-   hi
+  <Navbar />
  </div>
   );
 }
