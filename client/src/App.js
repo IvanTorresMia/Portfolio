b749da35cd20da8e5a5178d5e1a9a5d6caa11b27
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-10 12:47:15 -0800

    added particles and styling for it

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Components/Navbar'
-import Header from './Components/Header'
-;
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
 function App() {
   return (
- <div>
-  <Navbar />
- </div>
+    <>
+      <Particles
+      params={{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 6,
+              color: "#156307"
+            }
+          }
+        }
+      }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
-
+import Typed from 'react-typed'
 
 const Header = () => {
 
     return (
         <div className="header-wraper">
             <div className="main-info">
-                <h1>Full Stack Develper</h1>
+                <h1>Developer</h1>
+                <Typed
+                className="typed-text"
+                strings={["Full Stack MERN", "Web Developement", "Software Engineer"]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+                />
+                <a href="#" className="btn-main-offer">Contact Me</a>
             </div>
         </div>
     )
