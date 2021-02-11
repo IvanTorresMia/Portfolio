c2089c8faff072232e7e586bde378eaefbe4f7d0
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-11 14:26:49 -0800

    styling skill Items

--- client/src/Components/Skills/index.js
+++ client/src/Components/Skills/index.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import AllSkills from '../../utils/skills'
+import React from "react";
+import AllSkills from "../../utils/skills";
 
 const Skills = () => {
+  const skills1 = AllSkills[0].skills1;
+  const skills2 = AllSkills[0].skills2;
 
-    console.log(AllSkills[0].skills1)
-
-    return (
-        <div>
-            
+  return (
+    <div className="jumbotron skills-wraper">
+      <div className="skills-container container">
+        <div className="row">
+          <div className="col">
+            {skills1.map((skill, i) => (
+              <p key={i} className="rounded skill-item1">
+                {skill}
+              </p>
+            ))}
+          </div>
+          <div className="col">
+            {skills2.map((skill, i) => (
+              <p key={i} className="rounded skill-item2">
+                {skill}
+              </p>
+            ))}
+          </div>
         </div>
-    )
-}
+        <div className="col"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Skills;
