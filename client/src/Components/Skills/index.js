6b224dbc925682f60f6d5bf310549292efccbc65
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-11 17:03:48 -0800

    getting results from skills js

--- client/src/Components/Skills/index.js
+++ client/src/Components/Skills/index.js
@@ -1,28 +1,63 @@
 import React from "react";
 import AllSkills from "../../utils/skills";
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import profile from "../../profile.jpg";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 220,
+  },
+}));
+
+
 
 const Skills = () => {
-  const skills1 = AllSkills[0].skills1;
-  const skills2 = AllSkills[0].skills2;
+  const classes = useStyles();
+  const [secondary, setSecondary] = React.useState(false);
+  const [dense, setDense] = React.useState(false);
+  const skiils1 = AllSkills[0]
 
-
-
+  console.log(skiils1)
 
   return (
     <div className="jumbotron skills-wraper">
       <div className="skills-container container">
-       
+        <div className={classes.root}>
+          <List dense={dense}>
+            {/* mapp items here */}
+            <h3 className="list-title">Skills</h3>
+            <ListItem className="list-item">
+              <ListItemAvatar>
+                <Avatar alt="git hub" src={profile} />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Single-line item"
+                secondary={secondary ? "Secondary text" : null}
+              />
+            </ListItem>
+            <ListItem className="list-item">
+              <ListItemAvatar>
+                <Avatar alt="git hub" src={profile} />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Single-line item"
+                secondary={secondary ? "Secondary text" : null}
+              />
+            </ListItem>
+          </List>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Skills;
-
-
 
 // <div className="row">
 // <div className="col">
@@ -41,3 +76,18 @@ export default Skills;
 // </div>
 // </div>
 // <div className="col"></div>
+
+{
+  /* <List className={classes.demo}>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                    <Avatar alt="git hub" src={profile} />
+                </ListItemAvatar>
+                <ListItemText
+                primary="REACT JS"
+                secondary={secondary ? 'Secondary text' : null}
+                />
+              </ListItem>
+          </List>
+      </div> */
+}
--- client/src/utils/skills.js
+++ client/src/utils/skills.js
@@ -1,14 +1,111 @@
- const AllSkills = [{
-  skills1: ["HTML", "CSS", "ES6", "JavaScript", "React Js", "jQuery"],
-  skills2: ["mySql", "Sequelize", "Mongo DB", "Axios", "Ajax", "Node Js"],
-  otherSkills: [
-    "Auto Mechanics",
-    "Computer Repair",
-    "Sales Associate",
-    "Leadership",
-    "Musician",
-    "Creative",
-  ],
-}];
+const AllSkills = [
+  {
+    skills1: [
+      {
+        Lenguage: "HTML",
+        img: "#",
+        alt: "HTML logo"
+      },
+      {
+        Lenguage: "CSS",
+        img: "",
+        alt: "HTML logo"
+      },
+      {
+        Lenguage: "JavaScript",
+        img: "#",
+        alt: "HTML logo"
+      },
+      {
+        Lenguage: "React Js",
+        img: "#",
+        alt: "HTML logo"
+      },
+      {
+        Lenguage: "jQuery",
+        img: "#",
+        alt: "HTML logo"
+      },
+      {
+        Lenguage: "Git",
+        img: "#",
+        alt: "HTML logo"
+      }
+    ],
+  },
+  {
+    skills2: [
+      {
+        Lenguage: "",
+        img: "#",
+        alt: "HTML logo"
+      },
+      {
+        Lenguage: "",
+        img: "#",
+        alt: "HTML logo"
+      },
+      {
+        Lenguage: "",
+        img: "#",
+        alt: "HTML logo"
+      },
+      {
+        Lenguage: "",
+        img: "#",
+        alt: "HTML logo"
+      },
+      {
+        Lenguage: "",
+        img: "#",
+        alt: "HTML logo"
+      },
+      {
+        Lenguage: "",
+        img: "#",
+        alt: "HTML logo"
+      }
+    ],
+  },
+  {
+    otherSkills: [
+      {
+        Lenguage: "",
+        img: "#",
+      },
+      {
+        Lenguage: "",
+        img: "#",
+      },
+      {
+        Lenguage: "",
+        img: "#",
+      },
+      {
+        Lenguage: "",
+        img: "#",
+      },
+      {
+        Lenguage: "",
+        img: "#",
+      },
+      {
+        Lenguage: "",
+        img: "#",
+      }
+    ],
+  },
+];
 
 export default AllSkills;
+
+// skills1: ["HTML", "CSS", "ES6", "JavaScript", "React Js", "jQuery"],
+// skills2: ["mySql", "Sequelize", "Mongo DB", "Axios", "Ajax", "Node Js"],
+// otherSkills: [
+//   "Auto Mechanics",
+//   "Computer Repair",
+//   "Sales Associate",
+//   "Leadership",
+//   "Musician",
+//   "Creative",
+// ],
