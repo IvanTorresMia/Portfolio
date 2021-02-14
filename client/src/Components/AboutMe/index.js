7807615aa3e8ff0bf84ab44f32b1ce0626c60a2a
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-13 22:56:41 -0800

    fixing nav bar title

--- client/src/Components/AboutMe/index.js
+++ client/src/Components/AboutMe/index.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
       <div className="container">
         <div className="row">
           <div className="col">
-            <h1 className="">About Me</h1>
+            <h1>About Me</h1>
             <p className="lead">Let's change the world through Tech</p>
             <hr />
             <p className="about-me-text">
--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -43,7 +43,7 @@ const Navbar = () => {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Expirience
+                Experience
               </a>
             </li>
             <li className="nav-item">
