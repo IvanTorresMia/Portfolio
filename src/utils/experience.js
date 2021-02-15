890eb4ce96f972aebf6b6ecffeae17e7b9387129
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-15 13:29:31 -0800

    mkaing Experience js file and components

---
+++ src/utils/experience.js
@@ -0,0 +1,34 @@
+const Experience = [
+  {
+    title: "Graduated High School",
+    place: "Joliet Central High School",
+    date: "2010-2014",
+    description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+  },
+  {
+    title: "Graduated High School",
+    place: "Joliet Central High School",
+    date: "2010-2014",
+    description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+  }, 
+  {
+    title: "Graduated High School",
+    place: "Joliet Central High School",
+    date: "2010-2014",
+    description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+  },
+  {
+    title: "Graduated High School",
+    place: "Joliet Central High School",
+    date: "2010-2014",
+    description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+  },
+  {
+    title: "Graduated High School",
+    place: "Joliet Central High School",
+    date: "2010-2014",
+    description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+  },
+];
+
+export default Experience;
