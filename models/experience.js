586cbdaed265fb652c75fef7c05bf9acd2ca5a01
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-15 12:31:40 -0800

    finshed experience model

--- models/experience.js
+++ models/experience.js
@@ -3,11 +3,12 @@ module.exports = function(sequelize, DataTypes) {
     const Experience = sequelize.define("Experience", {
 
         title: DataTypes.STRING,
-        timeline1: DataTypes.INTEGER,
-        timeline2: DataTypes.INTEGER
+        date: DataTypes.STRING,
+        description: DataTypes.STRING
 
 
     })
     return Experience;
 }
+
 
