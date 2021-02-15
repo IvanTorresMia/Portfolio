d8324be38d3e9b17e188c2cf18dbf9d2eeddb669
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-15 12:31:57 -0800

    made route for experience data

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
 const db = require("../models")
 const router = require("express").Router()
 
+
+// getting all data from experience data base.
+router.get("/api/Experience", (req, res) => {
+    db.Experience.findAll({}).then(dbExperience => {
+        res.json(dbExperience);
+    }).catch(err => res.status(422).json(err))
+});
+
+modeule.exports = router;
