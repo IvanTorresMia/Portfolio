0e7b571932be7c613ffea5430830e6fcf87f1e8b
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-17 21:29:08 -0800

    added correct infor for resume section

--- src/utils/experience.js
+++ src/utils/experience.js
@@ -1,39 +1,41 @@
-import logo from '../skillLogos/grad.png'
+import logo from '../skillLogos/grad.png';
+import code from '../skillLogos/coding1.png';
+import tool from '../skillLogos/tool1.png';
 
 const Experience = [
   {
-    title: "Graduated High School",
-    place: "Joliet Central High School",
-    date: "2010-2014",
-    description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+    title: "Coding Bootcamp Graduate",
+    place: "Berkeley Extension Bootcamp",
+    date: "2020",
+    description: "Graduated with an A+ Over All Grade",
     logo: logo
   },
   {
-    title: "Graduated High School",
-    place: "Joliet Central High School",
-    date: "2010-2014",
-    description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+    title: "Leadership School Graduate",
+    place: "Bethel School of Ministry",
+    date: "2017-2020",
+    description: "Some of my best three years growing in confidence and leadership Skills",
     logo: logo
   }, 
   {
-    title: "Graduated High School",
-    place: "Joliet Central High School",
-    date: "2010-2014",
-    description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+    title: "Auto Mechanic",
+    place: "Torres Auto Repair",
+    date: "2014-2017",
+    description: "3 Years Experience with repairing automobiles and costumer service",
     logo: logo
   },
   {
-    title: "Graduated High School",
+    title: "High School Graduate",
     place: "Joliet Central High School",
     date: "2010-2014",
-    description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+    description: "Spent my highschool life exploring options, took A+ Certification, intro to Visual Basic Code, and other engineering classes.",
     logo: logo
   },
   {
-    title: "Graduated High School",
-    place: "Joliet Central High School",
+    title: "Computer Repair",
+    place: "Hobbie",
     date: "2010-2014",
-    description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+    description: "On my spare time I liked fixing Machintosh Computers",
     logo: logo
   },
 ];
