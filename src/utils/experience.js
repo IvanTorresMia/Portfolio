993cd95b2d4423430729f406d2cd4117d1ba0719
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-17 19:18:04 -0800

    imported image to file

--- src/utils/experience.js
+++ src/utils/experience.js
@@ -1,33 +1,40 @@
+import logo from '../skillLogos/grad.png'
+
 const Experience = [
   {
     title: "Graduated High School",
     place: "Joliet Central High School",
     date: "2010-2014",
     description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+    logo: logo
   },
   {
     title: "Graduated High School",
     place: "Joliet Central High School",
     date: "2010-2014",
     description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+    logo: logo
   }, 
   {
     title: "Graduated High School",
     place: "Joliet Central High School",
     date: "2010-2014",
     description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+    logo: logo
   },
   {
     title: "Graduated High School",
     place: "Joliet Central High School",
     date: "2010-2014",
     description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+    logo: logo
   },
   {
     title: "Graduated High School",
     place: "Joliet Central High School",
     date: "2010-2014",
     description: "Above 3.0 grade point average student. Was heavily involved in technology and music classes i.e. Visual Basic Programming, A + Cetification, Choir, and Guitar Club",
+    logo: logo
   },
 ];
 
