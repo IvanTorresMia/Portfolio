7bd3d7beabefb429a614b49f8347915d1f9adfa8
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-17 19:17:48 -0800

    added image to the card

--- src/Components/Experience/TimelineItem/index.js
+++ src/Components/Experience/TimelineItem/index.js
@@ -4,6 +4,7 @@ const TimelineItem = ({ Experience }) => {
   return (
     <div className="timeline-item">
       <div className="timeline-item-content">
+          <img className="timeline-item-image" src={Experience.logo} alt={Experience.title} style={{width: "4.5rem"}}/>
         <h3>{Experience.date}</h3>
         <h4>{Experience.title}</h4>
         <p>{Experience.place}</p>
