62dc9a03a017c3e877df58181c209337d8332f92
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-17 18:33:07 -0800

    fixing circle in component

--- src/Components/Experience/TimelineItem/index.js
+++ src/Components/Experience/TimelineItem/index.js
@@ -1,20 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const TimelineItem = ({ Experience }) => {
-    return (
-        <div className="timeline-item">
-        <div className="timeline-item-content">
-            <h3>{Experience.title}</h3>
-            <time>{Experience.date}</time>
-            <p>{Experience.place}</p>
-            <p>{Experience.description}</p>
-
-        </div>
+  return (
+    <div className="timeline-item">
+      <div className="timeline-item-content">
+        <h3>{Experience.title}</h3>
+        <time>{Experience.date}</time>
+        <p>{Experience.place}</p>
+        <p>{Experience.description}</p>
         <span className="circle"></span>
+      </div>
     </div>
-    )
-   
-}
-
+  );
+};
 
 export default TimelineItem;
