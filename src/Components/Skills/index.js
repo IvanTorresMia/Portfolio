d0b36d02692cbe8d363909f3082e747b5e7bff20
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-19 20:32:21 -0800

    integrading aos to skills

--- src/Components/Skills/index.js
+++ src/Components/Skills/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect} from "react";
+import Aos from 'aos';
+import "aos/dist/aos.css";
 import MySkills from "../../utils/skills";
 
 const Skills = () => {
@@ -7,6 +9,10 @@ const Skills = () => {
   const skills1 = MySkills[0].skills1;
   const skills2 = MySkills[1].skills2;
 
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  
+  }, [])
 
   return (
     <div className="Jumbotron skills-wraper">
@@ -14,7 +20,7 @@ const Skills = () => {
         <h1 className="skills-title">Skills</h1>
         <div className="row row-cols-1 row-cols-md-3">
           {skills1.map((skill, i) => (
-            <div key={i} className="col mb-4" style={{width: "15rem"}}>
+            <div key={i} className="col mb-4" style={{width: "15rem"}} data-aos="flip-up">
               <div className="h-100">
                 <img src={skill.img} className="card-img-top rounded" alt={skill.alt} />
                 <div className="card-body">
@@ -29,7 +35,7 @@ const Skills = () => {
         </div>
         <div className="row row-cols-1 row-cols-md-3">
           {skills2.map((skill, i) => (
-            <div key={i} className="col mb-4" style={{width: "15rem"}}>
+            <div key={i} className="col mb-4" style={{width: "15rem"}} data-aos="flip-down">
               <div className="h-100">
                 <img src={skill.img} className="card-img-top rounded" alt={skill.alt} />
                 <div className="card-body">
