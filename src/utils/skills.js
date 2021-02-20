be582dbf48be7917ff94587cdb114b6f254b15d1
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-19 20:35:38 -0800

    made files and folders for Portfolio Section

--- src/utils/skills.js
+++ src/utils/skills.js
@@ -54,13 +54,3 @@ const MySkills = [
 
 export default MySkills;
 
-// skills1: ["HTML", "CSS", "ES6", "JavaScript", "React Js", "jQuery"],
-// skills2: ["mySql", "Sequelize", "Mongo DB", "Axios", "Ajax", "Node Js"],
-// otherSkills: [
-//   "Auto Mechanics",
-//   "Computer Repair",
-//   "Sales Associate",
-//   "Leadership",
-//   "Musician",
-//   "Creative",
-// ],
