f497a4025d1e2ec065a4f6324652e6abfdfc023b
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-19 20:32:09 -0800

    integrating aos to timeline

--- src/Components/Experience/TimelineItem/index.js
+++ src/Components/Experience/TimelineItem/index.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const TimelineItem = ({ Experience }) => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <div className="timeline-item">
       <div className="timeline-item-content">
-          <img className="timeline-item-image" src={Experience.logo} alt={Experience.title} style={{width: "4.5rem"}}/>
-        <h3>{Experience.date}</h3>
-        <h4>{Experience.title}</h4>
-        <p>{Experience.place}</p>
-        <p>{Experience.description}</p>
+        <img
+          className="timeline-item-image"
+          src={Experience.logo}
+          alt={Experience.title}
+          style={{ width: "4.5rem" }}
+        />
+        <div data-aos="fade-up">
+          <h3>{Experience.date}</h3>
+          <h4>{Experience.title}</h4>
+          <p>{Experience.place}</p>
+          <p>{Experience.description}</p>
+        </div>
+
         <span className="circle"></span>
       </div>
     </div>
