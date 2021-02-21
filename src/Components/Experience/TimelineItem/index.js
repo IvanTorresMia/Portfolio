72184883f119d3b983dc44a9e07ea4314a287fdc
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-20 20:09:28 -0800

    deleted logo section

--- src/Components/Experience/TimelineItem/index.js
+++ src/Components/Experience/TimelineItem/index.js
@@ -10,19 +10,13 @@ const TimelineItem = ({ Experience }) => {
   return (
     <div className="timeline-item"  data-aos="zoom-in-up">
       <div className="timeline-item-content">
-        <img
-          className="timeline-item-image"
-          src={Experience.logo}
-          alt={Experience.title}
-          style={{ width: "4.5rem" }}
-        />
         <div data-aos="fade-up">
           <h3>{Experience.date}</h3>
           <h4>{Experience.title}</h4>
           <p>{Experience.place}</p>
           <p>{Experience.description}</p>
         </div>
-
+ 
         <span className="circle"></span>
       </div>
     </div>
