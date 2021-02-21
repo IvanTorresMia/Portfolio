36a8a47ee546cdb32aa94eacfe555afbbc10264f
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-21 00:27:50 -0800

    fixing colums

--- src/Components/Portfolio/Portfolio-Item/index.js
+++ src/Components/Portfolio/Portfolio-Item/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 const PortfolioItem = ({ Projects }) => {
 
     return (
-        <div className="col-sm-4">
-            <h3>{Projects.name}</h3>
-            <img src={Projects.img} alt={Projects.name} style={{width: "14rem", height: "9rem"}}/>
+        <div className="col-sm-4 project-card" style={{backgroundImage: `url(${Projects.img})`}}>
+            <div className="card-hover">
+                <h3>{Projects.name}</h3>
+            </div>
         </div>
     )
 }
