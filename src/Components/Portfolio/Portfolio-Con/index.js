be3622577b813895bb5a5d207a513129a6fa4a84
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-20 20:21:32 -0800

    created component for projects

---
+++ src/Components/Portfolio/Portfolio-Con/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Portfolio = () => {
+
+    return (
+        <div className="portfolio-wraper">
+            <h1>Portfolio</h1>
+        </div>
+    )
+}
+
+export default Portfolio;
