2c4524502b95842530857facb29fa1555865e4c4
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-20 23:18:12 -0800

    creating cards for projects

--- src/Components/Portfolio/Portfolio-Con/index.js
+++ src/Components/Portfolio/Portfolio-Con/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import PortfolioItem from '../Portfolio-Item'
-import Projects from '../../../utils/Projects.js'
+import React from "react";
+import PortfolioItem from "../Portfolio-Item";
+import Projects from "../../../utils/Projects.js";
 
 const Portfolio = () => {
-
-    return (
-        <div className="portfolio-wraper">
-            <h1>Portfolio</h1>
-            {Projects.map((project, i) => (
-                <div className="row">
-                <PortfolioItem Projects={project} key={i}/>
-                </div>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="portfolio-wraper">
+      <h1>Portfolio</h1>
+      <div className="row card-group">
+        {Projects.map((project, i) => (
+          <PortfolioItem Projects={project} key={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
