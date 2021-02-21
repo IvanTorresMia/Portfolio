1ab908b656f2e7149b747eadce6d997767a60e73
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-21 00:27:34 -0800

    creating rows and columns

--- src/Components/Portfolio/Portfolio-Con/index.js
+++ src/Components/Portfolio/Portfolio-Con/index.js
@@ -5,12 +5,12 @@ import Projects from "../../../utils/Projects.js";
 const Portfolio = () => {
   return (
     <div className="portfolio-wraper">
-      <h1>Portfolio</h1>
-      <div className="row card-group">
-        {Projects.map((project, i) => (
-          <PortfolioItem Projects={project} key={i} />
-        ))}
-      </div>
+        <h1>Portfolio</h1>
+        <div className="row card-group card-wraper">
+          {Projects.map((project, i) => (
+            <PortfolioItem Projects={project} key={i} />
+          ))}
+        </div>
     </div>
   );
 };
