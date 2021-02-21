ec938998f505950b21db566af607d0835943d3f0
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-20 23:18:31 -0800

    made grid for projects

--- src/Components/Portfolio/Portfolio-Item/index.js
+++ src/Components/Portfolio/Portfolio-Item/index.js
@@ -3,8 +3,9 @@ import React from 'react';
 const PortfolioItem = ({ Projects }) => {
 
     return (
-        <div className="col">
+        <div className="col-sm-4">
             <h3>{Projects.name}</h3>
+            <img src={Projects.img} alt={Projects.name} style={{width: "14rem", height: "9rem"}}/>
         </div>
     )
 }
