1f5b77b5ea76efc9555f7d629f7cc923ebd55382
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-22 20:41:11 -0800

    added discrition section for projects

--- src/utils/Projects.js
+++ src/utils/Projects.js
@@ -1,48 +1,59 @@
-import Quiz from '../Project-pictures/Quiz.jpg';
-import Empoyee from '../Project-pictures/Employee.jpg';
-import Jeronivosh from '../Project-pictures/Jeronivosh.jpg';
-import Shrek from '../Project-pictures/Shrek.jpg';
-import Weather from '../Project-pictures/Weather.jpg';
-import Books from '../Project-pictures/Books.jpg';
-
+import Quiz from "../Project-pictures/Quiz.jpg";
+import Empoyee from "../Project-pictures/Employee.jpg";
+import Jeronivosh from "../Project-pictures/Jeronivosh.jpg";
+import Shrek from "../Project-pictures/Shrek.jpg";
+import Weather from "../Project-pictures/Weather.jpg";
+import Books from "../Project-pictures/Books.jpg";
 
 const Projects = [
   {
     name: "TimedQuiz",
     github: "https://github.com/IvanTorresMia/TimedQuiz-by-IvanTorres",
     Deployed: "https://ivantorresmia.github.io/TimedQuiz-by-IvanTorres/",
-    img: Quiz
+    img: Quiz,
+    discription:
+      "Music Timed Quiz mainly using, Vanilla JavaScript, Local Storage, DOM manipulation, and Bootstrap",
   },
   {
     name: "Google Books",
     github: "https://github.com/IvanTorresMia/google-books-search",
     Deployed: "https://googlelibrary-search.herokuapp.com/",
-    img: Books
+    img: Books,
+    discription:
+      "Book search engine app mainly using Mongo DB, React Js, Axios, third party API, Rect Router.",
   },
   {
     name: "Jeronivosh",
     github: "https://github.com/IvanTorresMia/JERONIVOSH",
     Deployed: "http://jeronivosh.herokuapp.com/",
-    img: Jeronivosh
+    img: Jeronivosh,
+    discription:
+      "Text based choose your own adventure novel story game. Mainly Using React Js, MongoDB, and Express",
   },
   {
     name: "Weather App",
     github: "https://github.com/IvanTorresMia/My-Weather-app-Ivan",
     Deployed: "https://ivantorresmia.github.io/My-Weather-app-Ivan/",
-    img: Weather
+    img: Weather,
+    discription:
+      "Weather App mainly using, jQuery, Ajax, Third party API, and local storage.",
   },
   {
     name: "Shrek Game",
     github: "https://github.com/IvanTorresMia/shrek-game-itorres",
     Deployed: "https://ivantorresmia.github.io/shrek-game-itorres/",
-    img: Shrek
+    img: Shrek,
+    discription:
+      "A clicky shrek game to test your memory. Mainly using React Js.",
   },
   {
-    name: "Empoyee Tracker",
+    name: "Empoyee Directory",
     github: "https://github.com/IvanTorresMia/Employee-Directory-itorres",
     Deployed: "https://ivantorresmia.github.io/Employee-Directory-itorres/",
-    img: Empoyee
-  }
+    img: Empoyee,
+    discription:
+      "Employee Directory searches and updates search when you type in the input. Mainly Using React Js.",
+  },
 ];
 
 export default Projects;
