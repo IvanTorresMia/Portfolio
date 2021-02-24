f699dcafa43f99fb343eac6de429956eec0c259d
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-24 12:58:50 -0800

    working on material ui snipets

--- README.md
+++ README.md
@@ -111,12 +111,80 @@ import Particles from "react-particles-js";
 npm install @material-ui/core
 ```
 
-###
-
--
+- Importing different components and styles from materia-ui
 
 ```
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+```
 
+- using styles
+
+```
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 395,
+    margin: "2rem",
+    textAlign: "center",
+    backgroundColor: "rgb(76, 76, 76)",
+    color: "#fff"
+  },
+  media: {
+    height: 140,
+  },
+});
+```
+
+- example card
+
+```
+// Once Imported you can start using the components and styles from material-ui
+
+ <div className="col-sm-4 card-container">
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={Projects.img}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {Projects.name}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              className="card-p"
+            >
+              {Projects.discription}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <a href={Projects.github} target="_blank" rel="noreferrer">
+            <img
+              className="img-thumbnail m-2"
+              src={githubLogo}
+              alt="GitHub Icon"
+            />
+          </a>
+          <a href={Projects.Deployed} target="_blank" rel="noreferrer">
+            <img
+              className="img-thumbnail m-2"
+              src={deployedLogo}
+              alt="GitHub Icon"
+            />
+          </a>
+        </CardActions>
+      </Card>
+    </div>
 ```
 
 ## Author
--- src/Components/Portfolio/Portfolio-Item/index.js
+++ src/Components/Portfolio/Portfolio-Item/index.js
@@ -1,16 +1,13 @@
-
-
-
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import githubLogo from '../../../Contact-Folder/github.jpg'
-import deployedLogo from '../../../Contact-Folder/deployed1.jpg'
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import githubLogo from "../../../Contact-Folder/github.jpg";
+import deployedLogo from "../../../Contact-Folder/deployed1.jpg";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
@@ -18,7 +15,7 @@ const useStyles = makeStyles({
     margin: "2rem",
     textAlign: "center",
     backgroundColor: "rgb(76, 76, 76)",
-    color: "#fff"
+    color: "#fff",
   },
   media: {
     height: 140,
@@ -29,50 +26,47 @@ const PortfolioItem = ({ Projects }) => {
   const classes = useStyles();
 
   return (
-      <div className="col-sm-4 card-container">
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={Projects.img}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {Projects.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p" className="card-p">
-           {Projects.discription}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      <a href={Projects.github} target="_blank" rel="noreferrer">
-              <img
-                className="img-thumbnail m-2"
-                src={githubLogo}
-                alt="GitHub Icon"
-              />
-            </a>
-            <a href={Projects.Deployed} target="_blank" rel="noreferrer">
-              <img
-                className="img-thumbnail m-2"
-                src={deployedLogo}
-                alt="GitHub Icon"
-              />
-            </a>
-      </CardActions>
-    </Card>
+    <div className="col-sm-4 card-container">
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={Projects.img}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {Projects.name}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              className="card-p"
+            >
+              {Projects.discription}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <a href={Projects.github} target="_blank" rel="noreferrer">
+            <img
+              className="img-thumbnail m-2"
+              src={githubLogo}
+              alt="GitHub Icon"
+            />
+          </a>
+          <a href={Projects.Deployed} target="_blank" rel="noreferrer">
+            <img
+              className="img-thumbnail m-2"
+              src={deployedLogo}
+              alt="GitHub Icon"
+            />
+          </a>
+        </CardActions>
+      </Card>
     </div>
   );
-}
-
+};
 
 export default PortfolioItem;
-
-
-
-
-
-
-
