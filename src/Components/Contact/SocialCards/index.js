09e9e48acfc71625c773e1a7c61e0922aa5443e0
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-23 19:44:15 -0800

    added styling for social cards

--- src/Components/Contact/SocialCards/index.js
+++ src/Components/Contact/SocialCards/index.js
@@ -1,16 +1,61 @@
-import React from 'react';
-import InstaPng from '../../Contact-Folder/instagram.jpg';
-import FacebookPng from '../../Contact-Folder/facebook.jpg';
-import LinkedinPng from '../../Contact-Folder/linkedIn.png';
-
+import React from "react";
+import InstaPng from "../../../Contact-Folder/instagram.jpg";
+import FacebookPng from "../../../Contact-Folder/facebook.jpg";
+import LinkedinPng from "../../../Contact-Folder/linkedIn.png";
+import githubPng from "../../../Contact-Folder/github.jpg";
 
 const SocialCards = () => {
-
-    return (
-        <div className="container">
-            
+  return (
+    <div className="container social-container">
+        <h1>I am Social</h1>
+      <div className="row">
+        <div className="col" id="card" data-aos="flip-left">
+          <a
+            href="https://github.com/IvanTorresMia"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="img-thumbnail m-2"
+              src={githubPng}
+              alt="GitHub Icon"
+            />
+          </a>
         </div>
-    )
-}
+        <div className="col" id="card" data-aos="flip-left">
+          <a
+            href="https://www.linkedin.com/in/ivan-torres-0828931b2/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="img-thumbnail m-2"
+              src={LinkedinPng}
+              alt="LinkedIn Icon"
+            />
+          </a>
+        </div>
+        <div className="col" id="card" data-aos="flip-right">
+          <a
+            href="https://www.facebook.com/ivan.torres.980"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img class="img-thumbnail m-2" src={FacebookPng} alt="CV Icon" />
+          </a>
+        </div>
+        <div className="col" id="card" data-aos="flip-right">
+          <a
+            href="https://www.instagram.com/torres.ar.ivan/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img class="img-thumbnail m-2" src={InstaPng} alt="CV Icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SocialCards;
--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import emailjs from "emailjs-com";
-import EmailPng from '../../Contact-Folder/email.png';
-import PhonePng from '../../Contact-Folder/phone.png';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import EmailPng from "../../Contact-Folder/email.png";
+import PhonePng from "../../Contact-Folder/phone.png";
+import SocialCards from "./SocialCards";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 AOS.init();
 const ContactForm = () => {
@@ -26,68 +27,61 @@ const ContactForm = () => {
           console.log(error.text);
         }
       );
-  }
+  };
 
   return (
-      <div className=" contact-wraper"> 
-    <form className="container" onSubmit={sendEmail} data-aos="fade-up">
-      <input type="hidden" name="contact_number" />
-      <div>
-        <h1 className="display-4 contact-title">Lets Connect!</h1>
-        <div className="row">
-          <div className="col">
-            <img
-              class="img-thumbnail m-2"
-              src={EmailPng}
-              alt="CV Icon"
-            />
-            <p className="lead">ivantorresmia27@gmail.com</p>
-          </div>
-          <div className="col">
-            <img
-              class="img-thumbnail m-2"
-              src={PhonePng}
-              alt="CV Icon"
-            />
-            <p className="lead">815-603-3297</p>
+    <div className=" contact-wraper">
+      <form className="container" onSubmit={sendEmail} data-aos="fade-up">
+        <input type="hidden" name="contact_number" />
+        <div>
+          <h1 className="display-4 contact-title">Lets Connect!</h1>
+          <div className="row">
+            <div className="col">
+              <img class="img-thumbnail m-2" src={EmailPng} alt="CV Icon" />
+              <p className="lead">ivantorresmia27@gmail.com</p>
+            </div>
+            <div className="col">
+              <img class="img-thumbnail m-2" src={PhonePng} alt="CV Icon" />
+              <p className="lead">815-603-3297</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="row form-group">
-        <label for="exampleInputEmail1">Name</label>
-        <input
-          name="name"
-          type="text"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-      </div>
-      <div className="row form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input
-          name="email"
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-      </div>
-      <div className="row form-group">
-        <label for="exampleFormControlTextarea1">Message</label>
-        <textarea
-          name="message"
-          class="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
-        ></textarea>
-      </div>
-      <button type="submit" value="send" className="btn bg-light">
-        Submit
-      </button>
-    </form>
+        <div className="row form-group">
+          <label for="exampleInputEmail1">Name</label>
+          <input
+            name="name"
+            type="text"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="row form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            name="email"
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="row form-group">
+          <label for="exampleFormControlTextarea1">Message</label>
+          <textarea
+            name="message"
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <button type="submit" value="send" className="btn bg-light">
+          Submit
+        </button>
+      </form>
+      <SocialCards />
     </div>
   );
-}
+};
 
 export default ContactForm;
