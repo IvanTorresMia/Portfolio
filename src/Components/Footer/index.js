6072d448624922dd0a0d1a02b88828d3327c859e
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-23 20:10:11 -0800

    created footer

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const Footer = () => {
+
+    return (
+        <footer className="footer-wraper top-line">
+        <div className="footer navbar-dark bg-dark">
+            <div className="footer-copyright text-center py-3 text-muted">
+                © 2020 Copyright:
+                <a href="https://choosealicense.com/licenses/mit/">MIT licenses</a>
+            </div>
+            <div className="react-footer text-muted">
+            <h3>Made with React Js</h3>
+            </div>
+          
+        </div>
+    </footer>
+    )
+}
+
+export default Footer;
