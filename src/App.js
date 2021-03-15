a79dd55fc324e1009d5b3c33ecd52d796da0c7d1
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-15 12:50:34 -0700

    fixed padding on contact

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Timeline from './Components/Experience/Timeline'
 import Portfolio from './Components/Portfolio/Portfolio-Con'
 import ContactForm from './Components/Contact'
 import Footer from './Components/Footer'
-import { Link, animateScroll as scroll } from 'react-scroll'
 
 function App() {
   return (
